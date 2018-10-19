c70b34ffd2daa161a3affeecb033d39b789a5df6
Kabanga Bill <tkbillkabanga@gmail.com>
2018-10-19 11:49:28 +0300

    [feat] Implement functionality for login page[Finishes#161293798]

--- UI/scripts/script.js
+++ UI/scripts/script.js
@@ -1,14 +1,49 @@
 //grab the form from the dom
-let form = document.getElementById("loginForm");
-//get the value of the check box from the form using its name
-let clientInputs = form.querySelectorAll("input[name='clientType']");
+document.getElementById('loginForm').addEventListener('submit', loginUser);
+const url = 'http://127.0.0.1:5000/api/v1/auth/login';
 
-//listen for when the user changes the client type and update the form action accordingly
-clientInputs.forEach(function(input) {
-    input.addEventListener("change", function(event) {
-        //get the client value
-        let client = event.currentTarget.value;
-        let action=client === "admin" ? "admin.html":"user.html";
-        form.setAttribute("action",action);
-    });
-});
+function loginUser(e){
+    e.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let radioInput = document.getElementsByName('clientType');
+    for (let i = 0; i < radioInput.length; i++){
+        if (radioInput[i].checked){
+            role = radioInput[i].value;
+            break;
+        }
+    }
+    let data = {
+        username: username,
+        password: password,
+        role: role
+    }
+    fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+    .then(res => res.json())
+    .then(response => {
+        console.log(response);
+        if (response.message === 'Logged in successfully'){
+            token = response.access_token;
+            localStorage.setItem('token', token)
+            switch (role) {
+                case 'admin':
+                    window.location.replace('admin.html');
+                    break;
+                case 'client':
+                    window.location.replace('user.html');
+                    break;
+                default:
+                    break;
+            }
+            alert(`Welcome back ${username}`);
+        } else {
+            alert(response.message);
+        }
+    })
+    .catch(err => console.log(err));
+}
